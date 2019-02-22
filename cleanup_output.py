58e5c02d4b171334d1677736dd22855e5e6a801d
Tim Walter <tim@twwd.de>
2019-02-22 10:32:01 +0100

    Add merge functionality

--- cleanup_output.py
+++ cleanup_output.py
@@ -6,5 +6,18 @@ def process(filename):
         w.writelines(content)
 
 
-process("data/free.txt")
-process("data/occu.txt")
+def merge(in_files, out_file):
+    content = set()
+    for in_file in in_files:
+        with open(in_file, mode="r") as r:
+            content.update(r.readlines())
+    content = sorted(content)
+    with open(out_file, mode="w", newline="\n") as w:
+        w.writelines(content)
+
+
+merge(['data/free.txt', 'data/free_old.txt'], 'data/free.txt')
+merge(['data/occu.txt', 'data/occu_old.txt'], 'data/occu.txt')
+
+# process("data/free.txt")
+# process("data/occu.txt")

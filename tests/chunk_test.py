9d955194f3c2a1025a1f0eae55be9ef33f2ef90b
Tim Walter <tim@twwd.de>
2019-02-22 10:17:51 +0100

    Swap assertEqual parameters to match the error messages for expected and actual

--- tests/chunk_test.py
+++ tests/chunk_test.py
@@ -9,12 +9,12 @@ class TestPartitionValidator(unittest.TestCase):
     def test_simple_valid(self):
         testcases = [1, 2, 5, 2000]
         for testcase in testcases:
-            self.assertEqual(chunking.validator(testcase), testcase)
+            self.assertEqual(testcase, chunking.validator(testcase))
 
     def test_conversion(self):
         testcases = [1, 2, 5, 2000]
         for testcase in testcases:
-            self.assertEqual(chunking.validator(str(testcase)), testcase)
+            self.assertEqual(testcase, chunking.validator(str(testcase)))
 
     def test_simple_invalud(self):
         testcases = [0, -1, -205865]
@@ -26,9 +26,22 @@ class TestPartitionValidator(unittest.TestCase):
 class TestPatternGenerateCandidates(unittest.TestCase):
 
     def test_simple(self):
-        self.assertEqual(chunking.get([1, 2, 3], 1, 3), [1])
-        self.assertEqual(chunking.get([1, 2, 3], 2, 3), [2])
-        self.assertEqual(chunking.get([1, 2, 3], 3, 3), [3])
+        self.assertEqual([1], chunking.get([1, 2, 3], 1, 3))
+        self.assertEqual([2], chunking.get([1, 2, 3], 2, 3))
+        self.assertEqual([3], chunking.get([1, 2, 3], 3, 3))
+
+    def test_medium(self):
+        test_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual([0, 1], chunking.get(test_list, 1, 5))
+        self.assertEqual([2, 3], chunking.get(test_list, 2, 5))
+        self.assertEqual([4, 5], chunking.get(test_list, 3, 5))
+        self.assertEqual([6, 7], chunking.get(test_list, 4, 5))
+        self.assertEqual([8, 9], chunking.get(test_list, 5, 5))
+
+    def test_surplus(self):
+        test_list = [1, 2, 3, 4, 5]
+        self.assertEqual([1, 2], chunking.get(test_list, 1, 2))
+        self.assertEqual([3, 4, 5], chunking.get(test_list, 2, 2))
 
     def test_advanced(self):
         test_list = list(range(0, 20000))
@@ -38,7 +51,7 @@ class TestPatternGenerateCandidates(unittest.TestCase):
         for i in range(1, chunks + 1):
             result_list.extend(chunking.get(test_list, i, chunks))
 
-        self.assertEqual(result_list, test_list)
+        self.assertEqual(test_list, result_list)
 
 
 if __name__ == '__main__':
--- tests/pattern_test.py
+++ tests/pattern_test.py
@@ -9,7 +9,7 @@ class TestPatternValidator(unittest.TestCase):
     def test_simple_valid(self):
         testcases = ['abc.de', 'LLL.de', 'L-L.de', 'DDDDDabcd.de', 'AA.de']
         for testcase in testcases:
-            self.assertEqual(pattern.validator(testcase), testcase)
+            self.assertEqual(testcase, pattern.validator(testcase))
 
     def test_invalid_chars(self):
         testcases = ['ab.c', 'L%LL.de', 'Ä§""$%=($%.de']
@@ -28,8 +28,8 @@ class TestPatternValidator(unittest.TestCase):
             pattern.validator('LDajd-')
 
     def test_tld_adding(self):
-        self.assertEqual(pattern.validator('AA'), 'AA.de')
-        self.assertEqual(pattern.validator('abc'), 'abc.de')
+        self.assertEqual('AA.de', pattern.validator('AA'))
+        self.assertEqual('abc.de', pattern.validator('abc'))
 
     def test_to_short(self):
         with self.assertRaises(argparse.ArgumentTypeError):
@@ -43,27 +43,27 @@ class TestPatternValidator(unittest.TestCase):
 class TestPatternGenerateCandidates(unittest.TestCase):
 
     def test_simple(self):
-        self.assertEqual(pattern.generate_candiates('a.de'), ['a.de'])
-        self.assertEqual(pattern.generate_candiates('A.de'), ['{}.de'.format(c) for c in pattern.LETTERS + pattern.DIGITS])
+        self.assertEqual(['a.de'], pattern.generate_candiates('a.de'))
+        self.assertEqual(['{}.de'.format(c) for c in pattern.LETTERS + pattern.DIGITS], pattern.generate_candiates('A.de'))
 
     def test_no_dashes_at_start(self):
-        self.assertEqual(len(list(filter(lambda elem: elem[0] is '-', pattern.generate_candiates('AAA.de')))), 0)
+        self.assertEqual(0, len(list(filter(lambda elem: elem[0] is '-', pattern.generate_candiates('AAA.de')))))
 
     def test_no_dashes_at_end(self):
-        self.assertEqual(len(list(filter(lambda elem: elem[-3] is '-', pattern.generate_candiates('abA.de')))), 0)
+        self.assertEqual(0, len(list(filter(lambda elem: elem[-3] is '-', pattern.generate_candiates('abA.de')))))
 
     def test_generate_enough_candidates(self):
         len_digits = len(pattern.DIGITS)
         len_letters = len(pattern.LETTERS)
         len_arbitrary = len_letters + len_digits
 
-        self.assertEqual(len(pattern.generate_candiates('ahakjdgfdsghfgjfgb.de')), 1)
-        self.assertEqual(len(pattern.generate_candiates('D.de')), len_digits)
-        self.assertEqual(len(pattern.generate_candiates('L.de')), len_letters)
-        self.assertEqual(len(pattern.generate_candiates('A.de')), len_arbitrary)
-        self.assertEqual(len(pattern.generate_candiates('AA.de')), len_arbitrary * len_arbitrary)
-        self.assertEqual(len(pattern.generate_candiates('AAA.de')), len_arbitrary * (len_arbitrary + 1) * len_arbitrary)  # + 1 for dash
-        self.assertEqual(len(pattern.generate_candiates('aAA.de')), (len_arbitrary + 1) * len_arbitrary)  # + 1 for dash
+        self.assertEqual(1, len(pattern.generate_candiates('ahakjdgfdsghfgjfgb.de')))
+        self.assertEqual(len_digits, len(pattern.generate_candiates('D.de')))
+        self.assertEqual(len_letters, len(pattern.generate_candiates('L.de')))
+        self.assertEqual(len_arbitrary, len(pattern.generate_candiates('A.de')))
+        self.assertEqual(len_arbitrary * len_arbitrary, len(pattern.generate_candiates('AA.de')))
+        self.assertEqual(len_arbitrary * (len_arbitrary + 1) * len_arbitrary, len(pattern.generate_candiates('AAA.de')))  # + 1 for dash
+        self.assertEqual((len_arbitrary + 1) * len_arbitrary, len(pattern.generate_candiates('aAA.de')))  # + 1 for dash
 
 
 if __name__ == '__main__':
